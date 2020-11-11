eea08fdbe9d0f6ce84aad9a37447edeffce2afab
vadim <vadikoff57@gmail.com>
2020-11-11 14:13:16 +0300

    feat:"add two integration tests"

--- src/test/java/com/docsapp/app/DemoApplicationTests.java
+++
@@ -1,6 +0,0 @@
-package com.docsapp.app;
-
-class DemoApplicationTests {
-
-
-}
---
+++ src/test/java/com/docsapp/app/LoginTest.java
@@ -0,0 +1,40 @@
+package com.docsapp.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+
+public class LoginTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, guest")))
+                .andExpect(content().string(containsString("This is DocsApp Lab")));
+    }
+
+    @Test
+    public void accessDeniedTest() throws Exception {
+        this.mockMvc.perform(get("/main"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+}
